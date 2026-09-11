5a8ece578052e9a8a8a877b4972b05c4d93d0c8c
janvier <janviercardenas987@gmail.com>
2026-09-11 19:02:01 -0400

    Ejercicio 12

--- src/App.jsx
+++ src/App.jsx
@@ -3,82 +3,30 @@ import { useState } from "react";
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
-const Display = ({text}) => <h2>{text}</h2>
-
-const StatisticLine = (props) => {
-  return(
-    <tr>
-      <td>{props.text}</td>
-      <td>{props.value}</td>
-    </tr>
-  )
-}
-
-const Statistics = ({good, neutral, bad, all}) => {
-  
-  let average = all === 0 ? 0 :(good - bad) / all;
-  let positive = all === 0 ? 0 :(good / all) * 100
-
-  if(all === 0){
-    return <div>No feedback given</div>
-  }
-
-  return(
-    <div>
-      <table>
-        <tbody>
-          <StatisticLine text='good' value={good} />
-          <StatisticLine text='neutral' value={neutral} />
-          <StatisticLine text='bad' value={bad} />
-          <StatisticLine text='all' value={all} />
-          <StatisticLine text='average' value={average} />
-          <StatisticLine text='positive' value={positive} />
-        </tbody>
-      </table>
-    </div>
-  
-
-)}
-    
-  
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const increaseGood = () => { 
-    const updateGood = good + 1
-    setGood(updateGood)
-    setAll(updateGood + neutral + bad)
-  }
-  const increaseNeutral = () => {
-    const updateNeutral = neutral + 1;
-    setNeutral(updateNeutral)
-    setAll(good + updateNeutral + bad)
-  }
+  const[selected, setSelected] = useState(0);
 
-  const increaseBad = () => {
-    const updateBad = bad + 1
-    setBad(updateBad)
-    setAll(good + neutral + updateBad)
+  const generatedIndex = () => {
+    const tam = anecdotes.length;
+    let num = Math.floor(Math.random() * tam);
+    setSelected(num)
   }
-
-  
   return(
-    <div>
-      <Display text={'give feedback'} />
-
-      <Button onClick={increaseGood} text='good' />
-      <Button onClick={increaseNeutral} text='neutral' />
-      <Button onClick={increaseBad} text='bad' />
-
-      <Display text={'statistics'} />
-
-      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
-      
-
-    </div>
+    <article>
+      <p>{anecdotes[selected]}</p>
+      <Button onClick={generatedIndex} text={'next anecdote'}/>
+    </article>
   )
 }
 
