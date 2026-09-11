26bd4bff539ae3f6244e9f098268f34a4eb88616
janvier <janviercardenas987@gmail.com>
2026-09-10 20:07:57 -0400

    Ejercicio 1.7

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,45 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
   
-  const increaseGood = () => setGood(good + 1) 
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
+  const increaseGood = () => { 
+    const updateGood = good + 1
+    setGood(updateGood)
+    setAll(updateGood + neutral + bad)
+  }
+  const increaseNeutral = () => {
+    const updateNeutral = neutral + 1;
+    setNeutral(updateNeutral)
+    setAll(good + updateNeutral + bad)
+  }
+
+  const increaseBad = () => {
+    const updateBad = bad + 1
+    setBad(updateBad)
+    setAll(good + neutral + updateBad)
+  }
+
+  let average = all === 0 ? 0 :(good - bad) / all;
+  let positive = all === 0 ? 0 :(good / all) * 100
 
   return(
     <div>
       <Display text={'give feedback'} />
+
       <Button onClick={increaseGood} text='good' />
       <Button onClick={increaseNeutral} text='neutral' />
       <Button onClick={increaseBad} text='bad' />
+
       <Display text={'statistics'} />
+
       <Statistics name={'good'} count={good}/>
       <Statistics name={'neutral'} count={neutral}/>
       <Statistics name={'bad'} count={bad}/>
+      <Statistics name={'all'} count={all} />
+      <Statistics name={'average'} count={average} />
+      <Statistics name={'positive'} count={positive} />
+
     </div>
   )
 }
