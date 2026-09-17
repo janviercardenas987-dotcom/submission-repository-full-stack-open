e8ed0a88d888e3580b3df017d7b591befeaa7d03
janvier <janviercardenas987@gmail.com>
2026-09-16 20:22:00 -0400

    add: Exercise 2.1

---
+++ part2/submissionPart2/src/App.jsx
@@ -0,0 +1,31 @@
+import Header from "./Course/Header";
+import Content from "./Course/Content";
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+    </div>
+  );
+};
+
+export default App;
